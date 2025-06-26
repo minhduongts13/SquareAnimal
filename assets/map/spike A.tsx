a705a2134c1376168ccd279abe6306d1a7457e80
minhduongts13 <156182694+minhduongts13@users.noreply.github.com>
2025-06-26 15:57:32 +0700

    Updates

---
+++ assets/map/spike A.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="spike A" tilewidth="32" tileheight="32" tilecount="4" columns="2">
+ <image source="../images/obstacle/spike A.png" width="67" height="64"/>
+</tileset>
